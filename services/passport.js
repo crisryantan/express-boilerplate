a44ef6b73fdccf04a246658cddd1c35731a38502
Cris Ryan Tan <crisryantan@gmail.com>
2017-02-20 00:44:52 +0800

    add signin api

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,29 @@
-const passport    = require( 'passport' );
-const User        = require( '../models/user' );
-const config      = require( '../config' );
-const JwtStrategy = require( 'passport-jwt' ).Strategy;
-const ExtractJwt  = require( 'passport-jwt' ).ExtractJwt;
+const passport      = require( 'passport' );
+const User          = require( '../models/user' );
+const config        = require( '../config' );
+const JwtStrategy   = require( 'passport-jwt' ).Strategy;
+const ExtractJwt    = require( 'passport-jwt' ).ExtractJwt;
+const LocalStrategy = require( 'passport-local' );
+
+// create local strategy
+const localOptions = {
+	'usernameField' : 'email'
+};
+const localLogin = new LocalStrategy( localOptions, function( email, password, done ) {
+	// verify username and password.
+	User.findOne( { 'email' : email }, function ( err, user ) {
+		if ( err ) { return done( err ); }
+		if ( !user ) { return done( null, false ); }
+
+		// compare password - is 'password' equal to user.password
+		user.comparePassword( password, function ( err, isMatch ) {
+			if ( err ) { return done( err ); }
+			if ( !isMatch ) { return done( null, false ); }
+
+			return done( null, user );
+		} );
+	} );
+} );
 
 // setup options for JWT Strategy
 const jwtOptions = {
@@ -26,3 +47,4 @@ const jwtLogin = new JwtStrategy( jwtOptions, function ( payload, done ) {
 
 // tell passport to use this strategy
 passport.use( jwtLogin );
+passport.use( localLogin );
